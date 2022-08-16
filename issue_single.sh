12f3dd678688c6b3a9719593875ba706f3df5b32
Jeon Sangwan <maxswjeon@naver.com>
2022-08-16 13:13:50 +0900

    Remove personal email

--- issue_single.sh
+++ issue_single.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker exec certbot certbot certonly --dns-cloudflare --dns-cloudflare-credentials /opt/cloudflare/credentials --agree-tos -m maxswjeon@naver.com -d $1 --rsa-key-size 4096
-docker exec certbot certbot certonly --dns-cloudflare --dns-cloudflare-credentials /opt/cloudflare/credentials --agree-tos -m maxswjeon@naver.com -d $1 --key-type ecdsa --elliptic-curve secp384r1 --cert-name "${1}_ecc" 
+if [ ! -f .profile ]; then
+	echo "Enter email to use"
+	read MAIL
+	echo $MAIL > .profile
+else
+	MAIL=$(cat .profile)
+fi
+
+docker exec certbot certbot certonly --dns-cloudflare --dns-cloudflare-credentials /opt/cloudflare/credentials --agree-tos -m $MAIL -d $1 --rsa-key-size 4096
+docker exec certbot certbot certonly --dns-cloudflare --dns-cloudflare-credentials /opt/cloudflare/credentials --agree-tos -m $MAIL -d $1 --key-type ecdsa --elliptic-curve secp384r1 --cert-name "${1}_ecc" 
